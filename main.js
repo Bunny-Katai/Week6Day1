a8b2240b9f6ef433ff9ef2e79e413cea9cba6716
Supatchaya Smith <tai.pakdee@gmail.com>
2020-11-16 13:17:36 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,83 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+for (let i = 0; i < storeItems.length; i++){
+    if (storeItems[i].inStock){
+        $('#product-list-container').append(`<div class="list-item"><span>$${storeItems[i].price}</span><span>${storeItems[i].name}</span><span>${storeItems[i].details}</span></div>`)
+    }
+
+    
+}
+$('#dark-mode-toggle-button').click(function () {
+    $('#title').toggleClass('dark-title');
+    $('.list-item').toggleClass('dark-list-item');
+    $('#content-container').toggleClass('dark-container');
+    $('#dark-mode-toggle-button').toggleClass('dark-button');
+}); 
+
